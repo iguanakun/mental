b45acfd46b52913ed9bb4713e1bf1290326ef37b
iguanakun <yw16027@gmail.com>
2023-10-11 17:12:33 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
